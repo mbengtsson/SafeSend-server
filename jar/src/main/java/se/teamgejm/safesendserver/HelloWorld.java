38eea14238aa6011acb53bd39b854a4c83cb2194
Marcus Bengtsson <shiho.nuki@gmail.com>
2014-11-12 22:07:25 +0100

    Basic project stub in place

---
+++ jar/src/main/java/se/teamgejm/safesendserver/HelloWorld.java
@@ -0,0 +1,11 @@
+package se.teamgejm.safesendserver;
+
+/**
+ * Created by Marcus Bengtsson on 2014-11-12.
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
