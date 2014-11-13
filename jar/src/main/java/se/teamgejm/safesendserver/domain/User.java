ba09d5cb17a76729585b777357b7027b584df6e8
Marcus Bengtsson <shiho.nuki@gmail.com>
2014-11-13 13:58:42 +0100

    added user-entity stub

---
+++ jar/src/main/java/se/teamgejm/safesendserver/domain/User.java
@@ -0,0 +1,16 @@
+package se.teamgejm.safesendserver.domain;
+
+/**
+ * Created by Marcus Bengtsson on 2014-11-13.
+ */
+public class User {
+
+	private long id;
+
+	private String username;
+
+	private String password;
+
+	private String publicKey;
+	
+}
